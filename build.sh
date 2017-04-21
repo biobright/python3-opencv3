46b80f5305875ee2a0648f10b573d394fc2cd2ef
nate johnson <nate@biobright.org>
2017-04-21 11:46:13 -0400

    Make $USECUDA not case sensitive

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ DOCKERHUB_REPO='biobright/python3-opencv3'
 # allows env var to be overridden by shell, e.g. USECUDA=OFF ./build.sh
 : ${USECUDA:=OFF}
 
-if [ "${USECUDA}" = "ON" ]; then TAG='cuda'; else TAG='nocuda'; fi
-
-docker build -t ${DOCKERHUB_REPO}:${TAG} . \
+if [ "${USECUDA}" = "ON" ] || [ "${USECUDA}" = "on" ] ; then TAG='cuda'; else TAG='nocuda'; fi
+echo TAG IS: ${TAG}
+docker build --no-cache -t ${DOCKERHUB_REPO}:${TAG} . \
 && docker push ${DOCKERHUB_REPO}:${TAG}
